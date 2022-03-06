510468ab5d90edcb78c8308002a7ff3e730a6761
Kingrimursel <philipp.nazari@gmail.com>
2022-03-06 17:56:47 +0100

    implemented future_explosions_map into features and events

--- agent_code/my_agent/callbacks.py
+++ agent_code/my_agent/callbacks.py
@@ -25,7 +25,7 @@ def setup(self):
 
     if not os.path.isfile("my-saved-model.pt"):
         self.logger.info("Setting up model from scratch.")
-        self.model = np.ones((10, 10, 10, 10, 10, 6)) # one dimension for each feature and one for the actions
+        self.model = np.ones((10, 10, 10, 10, 10, 6))  # one dimension for each feature and one for the actions
 
     else:
         self.logger.info("Loading model from saved state.")
@@ -99,78 +99,30 @@ def state_to_features(self, game_state: dict) -> np.array:
         return None
 
     # numpyify the two lists for better access
-    bombs    = np.array(game_state["bombs"], dtype=object)
+    bombs = np.array(game_state["bombs"], dtype=object)
+
     opponents = np.array(game_state["others"], dtype=object)
+
+    future_explosions_map = create_future_explisions_map(bombs, game_state["field"])
+
 
     # coordinates of agents current position
     x,y = game_state["self"][-1]
 
-    current_field  = get_field_value(x,   y,   game_state, bombs, opponents)
-    left_field     = get_field_value(x-1, y,   game_state, bombs, opponents)
-    right_field    = get_field_value(x+1, y,   game_state, bombs, opponents)
-    top_field      = get_field_value(x,   y-1, game_state, bombs, opponents)
-    bottom_field   = get_field_value(x,   y+1, game_state, bombs, opponents)
+    current_field  = get_field_value(x,   y,   game_state, future_explosions_map, opponents)
+    left_field     = get_field_value(x-1, y,   game_state, future_explosions_map, opponents)
+    right_field    = get_field_value(x+1, y,   game_state, future_explosions_map, opponents)
+    top_field      = get_field_value(x,   y-1, game_state, future_explosions_map, opponents)
+    bottom_field   = get_field_value(x,   y+1, game_state, future_explosions_map, opponents)
 
-    #test = [current_field, left_field, right_field, top_field, bottom_field]
-    #print(test)
-    #print(tuple(test))
-
+    # save future_explosions_map to game_state
+    game_state["future_explosions_map"] = future_explosions_map  # TODO not working!
 
     return tuple([current_field, left_field, right_field, top_field, bottom_field])
 
 
-def look_for_targets(free_space, start, targets, logger=None):
-    """Find direction of the closest target that can be reached via free tiles.
 
-    Performs a breadth-first search of the reachable free tiles until a target is encountered.
-    If no target can be reached, the path that takes the agent closest to any target is chosen.
-
-    Args:
-        free_space: Boolean numpy array. True for free tiles and False for obstacles.
-        start: the coordinate from which to begin the search.
-        targets: list or array holding the coordinates of all target tiles.
-        logger: optional logger object for debugging.
-    Returns:
-        coordinate of first step towards the closest target or towards tile closest to any target.
-    """
-    if len(targets) == 0:
-        return None
-
-    frontier = [start]
-    parent_dict = {start: start}
-    dist_so_far = {start: 0}
-    best = start
-    best_dist = np.sum(np.abs(np.subtract(targets, start)), axis=1).min()
-
-    while len(frontier) > 0:
-        current = frontier.pop(0)
-        # Find distance from current position to all targets, track closest
-        d = np.sum(np.abs(np.subtract(targets, current)), axis=1).min()
-        if d + dist_so_far[current] <= best_dist:
-            best = current
-            best_dist = d + dist_so_far[current]
-        if d == 0:
-            # Found path to a target's exact position, mission accomplished!
-            best = current
-            break
-        # Add unexplored free neighboring tiles to the queue in a random order
-        x, y = current
-        neighbors = [(x, y) for (x, y) in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)] if free_space[x, y]]
-        shuffle(neighbors)
-        for neighbor in neighbors:
-            if neighbor not in parent_dict:
-                frontier.append(neighbor)
-                parent_dict[neighbor] = current
-                dist_so_far[neighbor] = dist_so_far[current] + 1
-    if logger: logger.debug(f'Suitable target found at {best}')
-    # Determine the first step towards the best found target tile
-    current = best
-    while True:
-        if parent_dict[current] == start: return current
-        current = parent_dict[current]
-
-
-def get_field_value(i, j, game_state, bombs, opponents):
+def get_field_value(i, j, game_state, future_explosions_map, opponents):
     """
 
     classify field (i, j) with its corresponding feature value
@@ -188,15 +140,15 @@ def get_field_value(i, j, game_state, bombs, opponents):
     # caution: order of lookup matters!
 
     # bomb
-    if len(bombs) > 0 and (i, j) in list(bombs[:, 0]):
-        return bombs[list(bombs[:, 0]).index((i, j))][1] + 2
+    if future_explosions_map[i, j] > 0:
+        return future_explosions_map[i, j] + 2
 
     # smoke
     elif game_state["explosion_map"][i, j] == 1:
         return 6
 
     # opponent
-    elif (i, j) in list(opponents[:, -1]):
+    elif opponents and (i, j) in list(opponents[:, -1]):
         return 7
 
     # coin
@@ -206,4 +158,83 @@ def get_field_value(i, j, game_state, bombs, opponents):
 
     # wall, crate, empty
     return game_state["field"][i, j]
+
+
+def create_future_explisions_map(bombs, arena):
+    """
+        Create map of future explisions
+
+        INPUT:
+            bombs: np.array of currently placed bombs
+            arena: current map
+        OUTPUT:
+            map of bombs and timers
+    """
+
+    width, height = arena.shape
+
+    arena_abs = np.abs(arena)
+
+    future_explosions_map = np.zeros((width, height), dtype=int)
+
+    # remember where non-free tiles are, since explosions get stopped there. removed later
+    future_explosions_map[arena != 0] = -10
+
+
+    # loop over all currently placed bombs
+    for (x, y), t in bombs:
+        for i in range(4):
+            if x - i > 0 and not wall_inbetween(x-i, y, x, y, arena_abs):
+                future_explosions_map[x-i, y] = t
+            if x + i < width and not wall_inbetween(x+i, y, x, y, arena_abs):
+                future_explosions_map[x+i, y] = t
+            if y - i > 0 and not wall_inbetween(x, y-i, x, y, arena_abs):
+                future_explosions_map[x, y-i] = t
+            if y + i < height and not wall_inbetween(x, y+i, x, y, arena_abs):
+                future_explosions_map[x, y+i]
+
+
+    # remove safe tiles again
+    future_explosions_map[future_explosions_map < 0] = 0
+
+    return future_explosions_map
+
+
+def wall_inbetween(x1, y1, x2, y2, arena_abs):
+    """
+    Checks if there is a wall inbetween (x1, y1) and (x2, y2)
+
+
+    INPUT:
+        xi, yi: coordinates
+        arena_abs: np.abs of field
+
+    OUTPUT:
+        boolean indicating whether there is a wall inbetwee the two tiles 
+    """
+
+    # bombs only explode along one coordinate axis. simulate this by returning true
+    if x1!=x2 and y1!=y2:
+        return False
+
+    return np.sum(arena_abs[x1:x2, y1:y2]) != 0
+
+
+
+
+def get_danger_level(i, j, future_explosions_map):
+    """
+    returns danger level on tile (i,j). Only determined by the bomb timer on this tile
+
+    INPUT:
+        i,j : position on map
+
+
+    OUTPUT:
+        possible bomb timer at position i,j
+
+    """
+
+    return future_explosions_map[i, j]
+
 
--- agent_code/my_agent/train.py
+++ agent_code/my_agent/train.py
@@ -6,7 +6,7 @@ import pickle
 from typing import List
 
 import events as e
-from .callbacks import state_to_features
+from .callbacks import state_to_features, get_danger_level, create_future_explisions_map
 
 # This is only an example!
 Transition = namedtuple('Transition',
@@ -17,7 +17,10 @@ TRANSITION_HISTORY_SIZE = 3  # keep only ... last transitions
 RECORD_ENEMY_TRANSITIONS = 1.0  # record enemy transitions with probability ...
 
 # Events
-PLACEHOLDER_EVENT = "PLACEHOLDER"
+PASSIVE_IN_DANGER_EVENT = "PASSIVE_IN_DANGER"
+ESCAPED_DANGER_EVENT    = "ESCAPED_DANGER"
+MOVED_INTO_DANGER_EVENT = "MOVED_INTO_DANGER"
+
 
 # action to int dit
 ACTIONS = {"UP": 0, "RIGHT": 1, "DOWN": 2, "LEFT": 3, "WAIT": 4, "BOMB": 5}
@@ -53,17 +56,42 @@ def game_events_occurred(self, old_game_state: dict, self_action: str, new_game_
     :param new_game_state: The state the agent is in now.
     :param events: The events that occurred when going from  `old_game_state` to `new_game_state`
     """
+
     self.logger.debug(f'Encountered game event(s) {", ".join(map(repr, events))} in step {new_game_state["step"]}')
 
-    if not old_game_state:  # TODO why can this be true?
-        print("yees")
+    # at the beginning of a round
+    if not old_game_state:
         return
+
+
+    # TODO: dont stay when in danger, and dont be in danger at the end
 
     # TODO: add reward when agent is next to a coin
 
-    # Idea: Add your own events to hand out rewards
-    #if ...:
-    #    events.append(PLACEHOLDER_EVENT)
+    old_position = old_game_state["self"][-1]
+    new_position = new_game_state["self"][-1]
+
+    # TODO: don't do there calculations twice. Save them somehow in the state_to_features callback
+    old_future_explosions_map = create_future_explisions_map(np.array(old_game_state["bombs"], dtype=object), old_game_state["field"])
+    new_future_explosions_map = create_future_explisions_map(np.array(new_game_state["bombs"], dtype=object), new_game_state["field"])
+
+
+    old_danger_level = get_danger_level(*old_position, old_future_explosions_map)
+    new_danger_level = get_danger_level(*new_position, new_future_explosions_map)
+
+    ## own events to hand out rewards
+
+    # if agent has not moved even though he was in danger
+    if old_position == new_position and old_danger_level != 0:
+            events.append(PASSIVE_IN_DANGER_EVENT)
+
+    # if agent has escaped a dangerous situation
+    if old_danger_level > 0 and new_danger_level == 0:
+        events.append(ESCAPED_DANGER_EVENT)
+
+    # agent got into danger from a safe position
+    if old_danger_level == 0 and new_danger_level > 0:
+        events.append(MOVED_INTO_DANGER_EVENT)
 
 
     # calculate current transition
@@ -76,11 +104,6 @@ def game_events_occurred(self, old_game_state: dict, self_action: str, new_game_
     old_features = state_to_features(self, old_game_state)
     new_features = state_to_features(self, new_game_state)
 
-    #print(new_features)
-    #print(old_game_state)
-    #print(self.model.shape)
-    #print(old_features)
-    #print(self.model[old_features].shape)
 
     old_model = self.model[old_features]
     new_model = self.model[new_features]
@@ -128,14 +151,17 @@ def reward_from_events(self, events: List[str]) -> int:
     certain behavior.
     """
     game_rewards = {
-        e.COIN_COLLECTED: 1,
+        e.COIN_COLLECTED: 4,
         e.KILLED_OPPONENT: 5,
-        e.CRATE_DESTROYED: 1,
-        e.COIN_FOUND: 2,
-        e.KILLED_SELF: -4,
-        e.OPPONENT_ELIMINATED: 3,
+        e.CRATE_DESTROYED: 2.5,
+        e.COIN_FOUND: 2.5,
+        e.KILLED_SELF: -6,
+        e.OPPONENT_ELIMINATED: 5,
+        e.INVALID_ACTION: -2,
         e.SURVIVED_ROUND: 4,
-        PLACEHOLDER_EVENT: -.1  # idea: the custom event is bad
+        PASSIVE_IN_DANGER_EVENT: -2,
+        ESCAPED_DANGER_EVENT: 4,
+        MOVED_INTO_DANGER_EVENT: -2.5
     }
 
     reward_sum = 0
