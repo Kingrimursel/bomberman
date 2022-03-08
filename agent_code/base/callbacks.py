fb8011ff1abc6fdc6ab8e53dd8bc9b549bb384af
eliasrieger <eliasrieger@gmx.de>
2022-03-08 15:23:42 +0100

    Finished Coin collector and base of Explorer.

--- agent_code/base/callbacks.py
+++ agent_code/base/callbacks.py
@@ -26,7 +26,7 @@ def setup(self):
         self.logger.info("Setting up model from scratch.")
         weights = np.random.rand(len(ACTIONS))
         self.model = np.ones((4,4,4,4,5,3,6))*[.25, .25, .25, .25, .0, .0] #Initial guess
-        
+
     else: #if model saved, no matter if in train mode or not, load current model #TODO: Why is this not done in the given code? In training mode a random model is picked always
         self.logger.info("Loading model from saved state.")
         with open("my-saved-model.pt", "rb") as file:
@@ -42,7 +42,7 @@ def act(self, game_state: dict) -> str:
     :param game_state: The dictionary that describes everything on the board.
     :return: The action to take as a string.
     """
-    
+
     random_prob = .2 #TODO: Adapt Exploration vs exploitation parameter
     if self.train and random.random() < random_prob:
         self.logger.debug("Choosing action purely at random.")
@@ -50,14 +50,14 @@ def act(self, game_state: dict) -> str:
         return np.random.choice(ACTIONS, p=[.25, .25, .25, .25, .0, .0])
 
     self.logger.debug("Querying model for action.")
-    
-    
-    
+
+
+
     features = state_to_features(self, game_state) #get features from game_state
     self.logger.info(f"FEATURE CALCULATED")
     #return np.random.choice(ACTIONS, p=self.model[0])
     return ACTIONS[np.argmax(self.model[features])] #Gives action with maximal reward for given state
-    
+
 
 #The following two functions are defined in here so they can also be used in the train.py
 def look_for_targets(free_space, start, targets, logger=None, dir=False):
@@ -104,22 +104,22 @@ def look_for_targets(free_space, start, targets, logger=None, dir=False):
         #if logger: logger.debug(f'Suitable target found with distance {best_dist}')
         # Determine the first step towards the best found target tile
         current = best
-        
+
         while True:
             if parent_dict[current] == start: return best_dist,current
             current = parent_dict[current]
-        
+
     return best_dist
 
 def destruction(arena, others_map, x,y):
     """
     Function that returns for a given tile, how much damage (crates/enemies) a bomb exploding at that time would create
-    
-    
+
+
     INPUT: (x,y) coordinates of tile
     OUTPUT: count of enemies/crates, that would be destroyed
     """
-    
+
     possible_destruction=0
     for (i, j) in [(x + h, y) for h in range(-3, 4)] + [(x, y + h) for h in range(-3, 4)]:
         if (0 < i< arena.shape[0]) and (0 < j< arena.shape[1]):
@@ -145,23 +145,23 @@ def state_to_features(self, game_state: dict) -> np.array:
 
     INPUT:
     game_state:  A dictionary describing the current game board.
-    
-    
+
+
     OUTPUT:
     tuple with 6 entries for the features
     """
-    
-    
-    
-    features = np.zeros(6, dtype='int')
-    
 
-    
-        
+
+
+    features = np.zeros(6, dtype='int')
+
+
+
+
     # This is the dict before the game begins and after it ends
     if game_state is None:
         return None
-    
+
     # Gather information about the game state
     arena = game_state['field']
     explosion_map = game_state['explosion_map']
@@ -171,28 +171,29 @@ def state_to_features(self, game_state: dict) -> np.array:
     bomb_xys = [xy for (xy, t) in bombs]
     others = [(n, s, b, xy) for (n, s, b, xy) in game_state['others']]
     coins = game_state['coins']
-    
+
     bomb_map = np.ones(arena.shape) * 5
     for (xb, yb), t in bombs:
         for (i, j) in [(xb + h, yb) for h in range(-3, 4)] + [(xb, yb + h) for h in range(-3, 4)]:
             if (0 < i < bomb_map.shape[0]) and (0 < j < bomb_map.shape[1]):
-                bomb_map[i, j] = min(bomb_map[i, j], t)                                             #Can be used as a measure for danger: 5 is undangerous, 0 is                                                                                    sure death
+                bomb_map[i, j] = min(bomb_map[i, j], t)                                             #Can be used as a measure for danger: 5 is undangerous, 0 is sure death
+
     bomb_positions = np.zeros(arena.shape) #1 where a bomb is placed, zero elsewhere
     for (xb, yb), t in bombs:
         bomb_positions[xb,yb] = 1
     others_map = np.zeros(arena.shape) #1 where another agent is placed, zero elsewhere
     for (n, s, b, xy) in others:
         others_map[xy[0],xy[1]] = 1
-    
+
     cols = range(1, arena.shape[0] - 1)
     rows = range(1, arena.shape[0] - 1)
     walls = [(x, y) for x in cols for y in rows if (arena[x, y] == -1)]
     crates = [(x, y) for x in cols for y in rows if (arena[x, y] == 1)]
     free_tiles = [(x, y) for x in cols for y in rows if (arena[x, y] == 0)]
     free_space = arena == 0 #For the function
-    
-    
-    
+
+
+
     #Phase Feature (5) (Needed for other features, because of that determined first)
     #Determine total number of found coins
     total_agents = 0 #TODO: Define depending on game mode, later always 3=opponent number)
@@ -200,15 +201,15 @@ def state_to_features(self, game_state: dict) -> np.array:
     for n,s,b,(x,y) in others:
         totalscore+=s
     totalcoins=totalscore-((total_agents-len(others))*5)
-    
+
     if(len(coins)>0):
         features[5]=0
     elif(totalcoins<9):
         features[5]=1
     else:
         features[5]=2
-    
-    
+
+
     #Neighbor tile features (0-3)
     #All the three are needed for the pase dependent decision
     distance_nextcoin = look_for_targets(free_space, (x, y), coins, self.logger) #distance to closest coin
@@ -216,7 +217,7 @@ def state_to_features(self, game_state: dict) -> np.array:
     distance_nextcrate = look_for_targets(free_space, (x, y), crates, self.logger) #distance to closest crate
     #how many crates/enemies would a bomb on this tile exploding right now destroy
     possible_destruction= destruction(arena, others_map, x,y)
-    
+
     for num, (i,j) in enumerate([(x + h, y) for h in [-1, 1]] + [(x, y + h) for h in [-1, 1]]):
         #if tile is free
         if (arena[i,j] == 0):
@@ -238,8 +239,8 @@ def state_to_features(self, game_state: dict) -> np.array:
             elif(features[5]==2):
                 if((look_for_targets(free_space,(i , j), [(xy) for (n, s, b, xy) in others], self.logger)<distance_nextopponent) and bomb_map[i,j]>4):
                     features[num]=3
-            
-        
+
+
 
     #Feature for current tile (4)
     #wait does not lead to sure death and (can not place bomb (current field is bomb or bomb was placed recently) or placing bomb leads to sure death(trapped))
@@ -254,10 +255,10 @@ def state_to_features(self, game_state: dict) -> np.array:
         features[4]=3
     if(bomb_map[x,y]==1 or bomb_positions[x,y]==1): #Bomb placed on current tile or wait is safe death
         features[4]=4
-    
-    
-    
-    
+
+
+
+
     output = tuple(features)
     #self.logger.debug(f'Features Calculated: {output}')
     return output
--- agent_code/user_agent/callbacks.py
+++ agent_code/user_agent/callbacks.py
@@ -1,7 +1,26 @@
+import numpy as np
 def setup(self):
     pass
 
 
 def act(self, game_state: dict):
+    # arena = game_state['field']
+    # explosion_map = game_state['explosion_map']
+    # step = game_state['step']
+    # bombs = game_state['bombs']
+    # bomb_xys = [xy for (xy, t) in bombs]
+    # n,s,b,(x, y) = game_state['self']
+    # others = [(n, s, b, xy) for (n, s, b, xy) in game_state['others']] #For calculating the number of coins collected yet
+    # coins = game_state['coins']
+    # bomb_map = np.ones(arena.shape) * 5
+    # for (xb, yb), t in bombs:
+    #     for (i, j) in [(xb + h, yb) for h in range(-3, 4)] + [(xb, yb + h) for h in range(-3, 4)]:
+    #         if (0 < i < bomb_map.shape[0]) and (0 < j < bomb_map.shape[1]):
+    #             bomb_map[i, j] = min(bomb_map[i, j], t)   #Can be used as a measure for danger: 5 is nothing, 0 is sure death
+    #
+    #
+    #
+    # print('Bomb Map: '+str(bomb_map))
+    # print('Explosion Map: '+str(explosion_map))
     self.logger.info('Pick action according to pressed key')
     return game_state['user_input']
